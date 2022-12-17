aac703cff8770c52ca1c9cc3eabedad20873fbd1
Dan <danpetersen@Alisons-MacBook-Pro.local>
2022-12-17 16:02:22 +0000

    vars added

--- src/APIs/unsplash.js
+++ src/APIs/unsplash.js
@@ -1,12 +1,14 @@
-require('dotenv').config()
+// import * as dotenv from 'dotenv'
 
-let key = process.env.UNSPLASH_KEY
+// dotenv.config()
+
+// let key = process.env.UNSPLASH_KEY
 
 const UnSplash = {
     baseURL: 'https://api.unsplash.com/search/photos',
     meta: {
         headers: {
-            "Authorization": key,
+            "Authorization": process.env.REACT_APP_UNSPLASH_KEY,
             "Method": "GET",
             "Content-Type": "application/json",
             "Accept": "application/json",
